a8ca8180f4a58d5e40dea65ba6d1dcd5407c65fd
Krishna <takemichihangaki753@gmail.com>
2026-09-18 15:43:41 +0530

    Update Another_Puzzle_from_Papyrus.cpp

--- Task 04: The Pirate King's Challenge/Another_Puzzle_from_Papyrus.cpp
+++ Task 04: The Pirate King's Challenge/Another_Puzzle_from_Papyrus.cpp
@@ -1,52 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long costWithoutReorder(vector<int>& a, vector<int>& b){
-    int n = a.size();
-    long long total = 0;
-
-    for(int i = 0; i < n; i++){
-        if (a[i] < b[i]) return -1; 
-        total += (a[i] - b[i]);
-    }
-    return total;
-}
-
-long long costWithReorder(vector<int> a, vector<int> b, int c){
-    int n = a.size();
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    long long total = c; 
-    for(int i = 0; i < n; i++){
-        if (a[i] < b[i]) return -1;
-        total += (a[i] - b[i]);
-    }
-    return total;
-}
-
-void solve(){
-    int n, c;
-    cin >> n >> c;
-    vector<int> a(n), b(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) cin >> b[i];
-    long long option1 = costWithoutReorder(a, b);
-    long long option2 = costWithReorder(a, b, c);
-    long long best = -1;
-    if(option1 != -1) best = option1;
-    if (option2 != -1){
-        if(best == -1 || option2 < best) best = option2;
-    }
-    cout << best << "\n";
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
     int t;
     cin >> t;
-    while(t--){
-        solve();
+
+    while (t > 0) {
+        t--;
+
+        int n, c;
+        cin >> n >> c;
+
+        vector<int> a(n);
+        vector<int> b(n);
+
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++) cin >> b[i];
+
+        // no reorder, match positions as given.
+        long long option1 = 0;
+        bool option1Possible = true;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] < b[i]) {
+                option1Possible = false;
+            } else {
+                option1 = option1 + (a[i] - b[i]);
+            }
+        }
+
+        // pay c, then sort both and match smallest-to-smallest.
+        vector<int> aSorted = a;
+        vector<int> bSorted = b;
+        sort(aSorted.begin(), aSorted.end());
+        sort(bSorted.begin(), bSorted.end());
+
+        long long option2 = c;
+        bool option2Possible = true;
+
+        for (int i = 0; i < n; i++) {
+            if (aSorted[i] < bSorted[i]) {
+                option2Possible = false;
+            } else {
+                option2 = option2 + (aSorted[i] - bSorted[i]);
+            }
+        }
+
+        // Pick the smaller valid option.
+        long long best = -1;
+
+        if (option1Possible) {
+            best = option1;
+        }
+
+        if (option2Possible) {
+            if (best == -1 || option2 < best) {
+                best = option2;
+            }
+        }
+
+        cout << best << "\n";
     }
+
     return 0;
 }
