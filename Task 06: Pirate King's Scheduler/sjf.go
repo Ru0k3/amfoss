a0e7f7491ed214796975c10fdceeef42cc49a338
Krishna <takemichihangaki753@gmail.com>
2026-09-18 11:32:50 +0530

    Update sjf.go

--- Task 06: Pirate King's Scheduler/sjf.go
+++ Task 06: Pirate King's Scheduler/sjf.go
@@ -1,48 +1,53 @@
 package main
 
-import "sort"
-
 func CalculateSJF(processes []Process) ([]ExecutionSlice, []Process) {
-	var completed []Process
-	var slices []ExecutionSlice
-	currentTime := 0
-	n := len(processes)
-	isCompleted := make([]bool, n)
+    var completed []Process
+    var slices []ExecutionSlice
+    currentTime := 0
+    n := len(processes)
+    isCompleted := make([]bool, n)
+    completedCount := 0
 
-	for i := 0; i < n; i++ {
-		minIndex := -1
-		for j, p := range processes {
-			if !isCompleted[j] && p.ArrivalTime <= currentTime {
-				if minIndex == -1 || p.BurstTime < processes[minIndex].BurstTime {
-					minIndex = j
-				}
-			}
-		}
+    for completedCount < n {
+        minIndex := -1
+        for j := 0; j < n; j++ {
+            if !isCompleted[j] && processes[j].ArrivalTime <= currentTime {
+                if minIndex == -1 || processes[j].BurstTime < processes[minIndex].BurstTime {
+                    minIndex = j
+                }
+            }
+        }
 
-		if minIndex == -1 {
-			currentTime++
-			continue
-		}
+        if minIndex == -1 {
+            currentTime++
+            continue
+        }
 
-		startTime := currentTime
-		endTime := currentTime + processes[minIndex].BurstTime
+        startTime := currentTime
+        endTime := currentTime + processes[minIndex].BurstTime
 
-		slices = append(slices, ExecutionSlice{
-			ProcessID: processes[minIndex].ID,
-			StartTime: startTime,
-			EndTime:   endTime,
-		})
+        slices = append(slices, ExecutionSlice{
+            ProcessID: processes[minIndex].ID,
+            StartTime: startTime,
+            EndTime:   endTime,
+        })
 
-		processes[minIndex].TurnaroundTime = endTime - processes[minIndex].ArrivalTime
-		processes[minIndex].WaitingTime = processes[minIndex].TurnaroundTime - processes[minIndex].BurstTime
-		isCompleted[minIndex] = true
-		completed = append(completed, processes[minIndex])
-		currentTime = endTime
-	}
+        processes[minIndex].TurnaroundTime = endTime - processes[minIndex].ArrivalTime
+        processes[minIndex].WaitingTime = processes[minIndex].TurnaroundTime - processes[minIndex].BurstTime
+        
+        isCompleted[minIndex] = true
+        completed = append(completed, processes[minIndex])
+        currentTime = endTime
+        completedCount++
+    }
 
-	sort.SliceStable(completed, func(i, j int) bool {
-		return completed[i].ID < completed[j].ID
-	})
+    for i := 0; i < len(completed); i++ {
+        for j := i + 1; j < len(completed); j++ {
+            if completed[i].ID > completed[j].ID {
+                completed[i], completed[j] = completed[j], completed[i]
+            }
+        }
+    }
 
-	return slices, completed
+    return slices, completed
 }
